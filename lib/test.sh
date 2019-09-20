75099793574c9e6b9e5a2335d704e102efeca911
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-20 17:49:13 -0400

    baseline

--- lib/test.sh
+++ lib/test.sh
@@ -6,22 +6,12 @@ p6_cicd__debug() {
     p6_debug "p6_cicd: $msg"
 }
 
-# XXX: p6common cannot dependon p6test -> p6cicd ?
 p6_cicd_tests_run() {
-
-    . ../p6test/lib/_bootstrap.sh
-    p6_p6test_bootstrap "../p6test"
 
     p6_test_harness_tests_run "t"
 }
 
 p6_cicd_test_benchmark() {
-
-    . ../p6common/lib/_bootstrap.sh
-    p6_bootstrap "../p6common"
-
-    . ../p6test/lib/_bootstrap.sh
-    p6_p6test_bootstrap "../p6test"
 
     p6_test_bench "$@"
 }
